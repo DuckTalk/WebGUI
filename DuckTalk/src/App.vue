81d421e5e129b4002e33e723ad765724620cd1ac
Alex12345005 <rath190134@sr.htlweiz.at>
2023-03-23 13:16:06 +0100

    edited_page_with_bootstrap

--- DuckTalk/src/App.vue
+++ DuckTalk/src/App.vue
@@ -5,46 +5,15 @@ import LoginView from './components/LoginView.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="DuckTalk Titel" class="logo" src="./assets/ducktalk2.png"/>
-    <img alt="DuckTalk Logo" class="logo" src="./assets/besticon.png" width="300" height="300" />
-  </header>
-
-  <main>
-    <LoginView />
-  </main>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <img class="d-block mx-auto" alt="DuckTalk Logo" src="./assets/besticon.png" width="250" height="250" />
+        <img class="d-block mx-auto" alt="DuckTalk Titel" src="./assets/ducktalk2.png" />
+      </div>
+      <div class="col"> 
+          <LoginView />
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  font-size: xx-large;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-.body{
-  background-color: #333;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- DuckTalk/src/components/LoginView.vue
+++ DuckTalk/src/components/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="login-container">
-      <h2>Login</h2>
+    <div class="container">
+      <h2 class="row">Login</h2>
       <form @submit.prevent="handleSubmit">
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" required>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required>
-        <button type="submit">Login</button>
+        <label class="row" for="email">Email:</label>
+        <input class="row" type="email" id="email" v-model="email" required>
+        <label class="row" for="password">Password:</label>
+        <input class="row" type="password" id="password" v-model="password" required>
+        <button class="row btn button bg-secondary" type="submit" >Login</button>
       </form>
     </div>
   </template>
@@ -27,66 +27,3 @@
     }
   };
   </script>
-  
-  <style scoped>
-  .login-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: #333;
-    font-family: Arial, sans-serif;
-  }
-  
-  h2 {
-    margin-top: 0;
-    margin-bottom: 1rem;
-    font-size: 2rem;
-    font-weight: bold;
-  }
-  
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    max-width: 400px;
-    padding: 2rem;
-    background-color: #fff;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  }
-  
-  label {
-    font-size: 1.2rem;
-    margin-bottom: 0.5rem;
-  }
-  
-  input[type="email"],
-  input[type="password"] {
-    padding: 0.5rem;
-    margin-bottom: 1rem;
-    width: 100%;
-    border-radius: 4px;
-    border: 1px solid #ccc;
-    font-size: 1.2rem;
-  }
-  
-  button[type="submit"] {
-    padding: 0.5rem 1rem;
-    margin-top: 1rem;
-    background-color: #0077cc;
-    color: #fff;
-    font-size: 1.2rem;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: all 0.2s ease;
-  }
-  
-  button[type="submit"]:hover {
-    background-color: #005fa3;
-  }
-  </style>
-  
