c1307dd31e10516fcbde0c2be2e91dfd3c731362
Alex12345005 <rath190134@sr.htlweiz.at>
2023-05-04 13:05:30 +0200

    started design

--- DuckTalk/src/App.vue
+++ DuckTalk/src/App.vue
@@ -3,17 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import LoginView from './components/LoginView.vue'
 import SignupView from './components/SignupView.vue';
+import LandingView from './components/LandingView.vue';
 </script>
 
 <template>
-  <div class="w-100 p-4 d-flex align-items-center justify-content-center" >
+  <div class="w-100 p-4 d-flex" >
     <div class="row mt-4 pt-4">
-      <div class="col">
-        <img class="d-block mx-auto" alt="DuckTalk Logo" src="./assets/besticon.png" width="250" height="250" />
-        <img class="d-block mx-auto" alt="DuckTalk Titel" src="./assets/ducktalk2.png" />
-      </div>
-      <div class="col p-5 d-flex align-items-center"> 
-          <SignupView />
+      <div class="col p-5 d-flex"> 
+          <LandingView />
       </div>
     </div>
   </div>
---
+++ DuckTalk/src/components/LandingView.vue
@@ -0,0 +1,177 @@
+<template>
+    <div class="container bg-dark">
+      <div class="row">
+        <div class="" id="chat_background">
+          <div class="card bg-secondary" id="card_bg_secondary">
+            <div class="card-header">
+              <h4 class="text-light">Contacts</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  <div class="contact">
+                    <img src="contact1.jpg" alt="Contact 1" class="contact-image">
+                    <div class="contact-details">
+                      <h5 class="text-light">Contact 1</h5>
+                      <p class="text-muted">Last message from Contact 1</p>
+                    </div>
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  <div class="contact">
+                    <img src="contact2.jpg" alt="Contact 2" class="contact-image">
+                    <div class="contact-details">
+                      <h5 class="text-light">Contact 2</h5>
+                      <p class="text-muted">Last message from Contact 2</p>
+                    </div>
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  <div class="contact">
+                    <div class="contact-details">
+                      <h5 class="text-light">Contact 3</h5>
+                      <p class="text-muted">Last message from Contact 3</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-8 align-text-bottom">
+          <div class="card bg-secondary" id="chatbox">
+            <div class="card-header">
+              <h4 class="text-light">Chat with Contact 1</h4>
+            </div>
+            <div class="card-body">
+              <ul class="chat">
+                <li class="chat-message incoming">
+                  <div class="chat-message-content">
+                    <p>Hello!</p>
+                  </div>
+                </li>
+                <li class="chat-message outgoing">
+                  <div class="chat-message-content">
+                    <p>Hi, how are you?</p>
+                  </div>
+                </li>
+                <li class="chat-message incoming">
+                  <div class="chat-message-content">
+                    <p>I'm good, thanks! How about you?</p>
+                  </div>
+                </li>
+              </ul>
+              <form class="chat-form" @submit.prevent="sendMessage">
+                <input type="text" class="form-control" v-model="message" placeholder="Type your message...">
+                <button type="submit" class="btn btn-success">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<style>
+
+#chat_background {
+    background-color: grey;
+}
+
+body {
+  background-color: #1c1c1c;
+  color: #fff;
+}
+
+.card-header {
+  background-color: #1c1c1c;
+  color: #00ff00;
+}
+
+.card
+
+.card-body {
+  background-color: #2b2b2b;
+  color: #fff;
+  width: 100%;
+  
+}
+
+.list-group-item {
+  background-color: #2b2b2b;
+  color: #fff;
+}
+
+.list-group-item:hover {
+  background-color: #3d3d3d;
+}
+
+.chat {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.chat-message {
+  margin-bottom: 20px;
+}
+
+.chat-message-content {
+  max-width: 60%;
+  padding: 10px 20px;
+  border-radius: 20px;
+}
+
+.incoming .chat-message-content {
+  background-color: #2b2b2b;
+  color: #fff;
+  float: left;
+}
+
+.outgoing .chat-message-content {
+  background-color: #00ff00;
+  color: #000;
+  float: right;
+}
+
+.chat-form {
+  margin-top: 650px;
+  display: flex;
+}
+
+.chat-form input[type="text"] {
+  flex: 1;
+  padding: 10px;
+  border-radius: 20px;
+  border: none;
+  background-color: #2b2b2b;
+  color: #fff;
+}
+
+.chat-form button {
+  padding: 10px 20px;
+  border-radius: 20px;
+  border: none;
+  background-color: #00ff00;
+  color: #000;
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+.chat-form button:hover {
+  background-color: #009900;
+}
+
+#card_bg_secondary{
+    margin-right: 50px;
+}
+
+
+#chatbox{
+    margin-left: 400px;
+}
+
+
+</style>
+
+  
