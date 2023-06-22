245923868c58d1dc3d1607015d14fc8275935e47
Alex12345005 <rath190134@sr.htlweiz.at>
2023-06-22 11:24:28 +0200

    added_post_message

--- DuckTalk/src/components/LandingView.vue
+++ DuckTalk/src/components/LandingView.vue
@@ -60,8 +60,8 @@
               </li>
             </ul>
             <form class="chat-form">
-              <input type="text" class="form-control" v-model="content">
-              <button type="button" class="btn btn-success" @click="testfunc">Send</button>
+              <input type="text" class="form-control" v-model="newMessage">
+              <button type="button" class="btn btn-success" @click="sendMessage">Send</button>
             </form>
           </div>
         </div>
@@ -75,85 +75,64 @@
 export default {
   data() {
     return {
-      message: 'Type your message...', // Add the 'message' property to the component's data
-      messages: {
-        user1: [],
-        user2: []
-      },
-      newMessage: ''
-    };
+    message: 'Type your message...',
+    messages: {
+      user1: [] as { id: number; text: string }[],
+      user2: [] as { id: number; text: string }[]
+    },
+    newMessage: ''
+  };
   },
   methods: {
-     sendMessage: function() {
-      try {
-      const r = await fetch('http://ableytner.ddns.net:2006/api/user/thetestuser1@mail.com', {
-        method: 'GET',
-        //mode: 'no-cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          Accept: 'application/json'
-        },
-      });
-      const json_obj = await r.json();
-      console.log(json_obj);
-    } catch (error) {
-      console.error('Error occoured', error);
-    }
-    }
 
-  },
-
-  async get_user() {
+  async sendMessage() {
+    var json_obj_1 = null;
+    var json_obj_2 = null;
     try {
-      const r = await fetch('http://ableytner.ddns.net:2006/api/user/thetestuser1@mail.com', {
+      const extra_option = {
         method: 'GET',
         //mode: 'no-cors',
         headers: {
           'Access-Control-Allow-Origin': '*',
           Accept: 'application/json'
-        },
-      });
-      const json_obj = await r.json();
-      console.log(json_obj);
+        }
+      }
+
+      const usr_1 = await fetch('http://ableytner.ddns.net:2006/api/user/thetestuser1@mail.com',extra_option);
+      const usr_2 = await fetch('http://ableytner.ddns.net:2006/api/user/thetestuser2@mail.com',extra_option);
+      json_obj_1 = await usr_1.json();
+      json_obj_2 = await usr_2.json();
+      console.log(json_obj_1);
+      console.log(json_obj_2);
+
     } catch (error) {
       console.error('Error occoured', error);
     }
-  },
 
-  async get_user2() {
-    try {
-      const r = await fetch('http://ableytner.ddns.net:2006/api/user/thetestuser2@mail.com', {
-        method: 'GET',
+
+
+
+    const extra_option_post = {
+        method: 'POST',
         //mode: 'no-cors',
         headers: {
           'Access-Control-Allow-Origin': '*',
-          Accept: 'application/json'
+          Accept: 'application/json',
+          'Content-type': 'application/json'
         },
-      });
-      const json_obj = await r.json();
-      console.log(json_obj);
-    } catch (error) {
-      console.error('Error occoured', error);
-    }
-  },
+        body: JSON.stringify({data: {
+        sender_id: json_obj_1["data"]["user_id"],
+        receiver: {
+          type: "user",
+          user_id: json_obj_2["data"]["user_id"]
+        },
+        content: this.newMessage
 
-  async sendMessageOld() {
-    // Determine the sender and recipient based on your logic
-    console.log("In Send Message");
-    const sender = 'user1';
-    const recipient = 'user2';
-
-    // Create a new message object
-    const message = {
-      id: Date.now(),
-      text: this.newMessage
-    };
-
-    // Add the message to the sender's array
-    this.messages[sender].push(message);
-
-    // Clear the input field
-    this.newMessage = '';
+      }})
+      }
+      const new_message = await fetch('http://ableytner.ddns.net:2006/api/message',extra_option_post);
+      console.log(new_message.json())
+  }
   }
 };
 </script>
